db0e687cb2e0c35cbac0690bfc62a038abd7a007
Anton Knyazev <knyazevmac@icloud.com>
2024-05-15 20:49:03 +0300

    script for create DB and update DB_Info.txt

---
+++ scripts/create_DB.py
@@ -0,0 +1,134 @@
+import sqlite3 as sq
+
+class CreateDataBase():
+    '''
+        Этот класс предназначен для создания базы данных
+    '''
+
+    def __init__(self) -> None:
+        '''
+            Конструктор класса CreateDataBase
+        '''
+
+        # Вызываем функцию, которая создает базу данных
+        self.__createDB()
+
+    def __createDB(self) -> None:
+        '''
+            Функция для создания базы данных
+        '''
+        
+        # Пробуем подключиться(или создать) к базе данных
+        try:
+            self.connection = sq.connect('DataBase.db')
+            self.cursor = self.connection.cursor()
+        
+        # Если что-то пошло не так, выведется ошибка
+        except sq.Error as error:
+            print(error)
+
+        # Если блок try не выдал исключений
+        else:
+            self.__createTables()
+
+
+    def __createTables(self) -> None:
+        '''
+            Функция для создания таблиц в базе данных
+        '''
+
+        # Пробуем создать таблицы
+        try:
+            # Создаем таблицу с должностями
+            self.cursor.execute("""
+
+                                CREATE TABLE IF NOT EXISTS job_title(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT NOT NULL,
+                                    salary INTEGER NOT NULL
+                                )
+
+                                """)
+            
+            self.cursor.execute("""
+
+                                CREATE TABLE IF NOT EXISTS department(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT NOT NULL
+                                )
+
+                                """)
+            
+            # Создаем таблицу с сотрудниками
+            self.cursor.execute("""
+
+                                CREATE TABLE IF NOT EXISTS employees(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT NOT NULL,
+                                    second_name TEXT NOT NULL,
+                                    surname TEXT NOT NULL,
+                                    login TEXT NOT NULL UNIQUE,
+                                    password TEXT NOT NULL,
+                                    age INTEGER NOT NULL,
+                                    date_birth TEXT NOT NULL,
+                                    status_vacation BOOLEAN NOT NULL,
+                                    status_retirement BOOLEAN NOT NULL,
+                                    status_pre_retirement BOOLEAN NOT NULL,
+                                    status_childless BOOLEAN NOT NULL,
+                                    status_many_children BOOLEAN NOT NULL,
+                                    status_veteran BOOLEAN NOT NULL
+                                )
+
+                                """)
+            
+            # Создаем таблицу с вакансиями
+            self.cursor.execute("""
+
+                                CREATE TABLE IF NOT EXISTS job_vacancy(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    id_job_title TEXT NOT NULL,
+                                    id_department INTEGER,
+                                    vacancy_opening_date TEXT NOT NULL,
+                                    vacancy_closing_date TEXT,
+
+                                    FOREIGN KEY (id_job_title) REFERENCES job_title (id) ON DELETE CASCADE,
+                                    FOREIGN KEY (id_department) REFERENCES department (id) ON DELETE CASCADE
+                                )
+                                
+                                """)
+            
+            # Создаем таблицу с сотрудниками и их должностями
+            self.cursor.execute("""
+
+                                CREATE TABLE IF NOT EXISTS employees_and_positions(
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    id_employee INTEGER NOT NULL,
+                                    id_job_title TEXT NOT NULL,
+                                    id_department INTEGER,
+                                
+                                    FOREIGN KEY (id_employee) REFERENCES employees (id) ON DELETE CASCADE,
+                                    FOREIGN KEY (id_job_title) REFERENCES job_title (id) ON DELETE CASCADE,
+                                    FOREIGN KEY (id_department) REFERENCES department (id) ON DELETE CASCADE
+                                )
+
+                                """)
+            
+            # Сохраняем таблицы
+            self.connection.commit()
+
+        # Выводим исключение, если оно есть
+        except sq.Error as error:
+            print(error)
+
+    def __del__(self) -> None:
+        '''
+            Деструктор класса CreateDataBase.\n
+            Отрабатывает тогда, когда данный класс удаляется из кэша сборщиком мусора
+        '''
+
+        # Закрываем все подключения
+        self.connection.close()
+
+
+if __name__ == '__main__':
+    CreateDataBase()
