3788f0f29ea247a2228cd6e92fe2eabd945a6b59
Anton Knyazev <knyazevmac@icloud.com>
2024-05-24 15:35:26 +0300

    completed window login

--- app/login.py
+++ app/login.py
@@ -17,7 +17,32 @@ class Login(tk.Tk):
     ) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
 
-        self.geometry("600x600")
+        self.geometry("370x420")
         self.title("Login")
+        self.resizable(height=False, width=False)
+
+        self.__add_widgets()
 
         self.mainloop()
+
+    def __add_widgets(self):
+        """
+        Функция для добавления виджетов на экран
+        """
+
+        logo_label = tk.Label(self, text="Вход", font=("Arial", 50))
+        label_for_login = tk.Label(self, text="Логин: ", font=("Arial", 22))
+        entry_for_login = tk.Entry(self, font=("Arial", 16))
+        label_for_password = tk.Label(self, text="Пароль: ", font=("Arial", 22))
+        entry_for_password = tk.Entry(self, show="*", font=("Arial", 16))
+        button_login = tk.Button(self, text="Войти")
+
+        logo_label.pack(pady=30)
+
+        label_for_login.pack()
+        entry_for_login.pack()
+
+        label_for_password.pack()
+        entry_for_password.pack()
+
+        button_login.pack(pady=15)
