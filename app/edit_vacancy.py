aab221d943b432314e9f2697ab718019f1fca326
Anton Knyazev <knyazevmac@icloud.com>
2024-06-22 17:03:32 +0300

    update design page of edit vacancy

--- app/edit_vacancy.py
+++ app/edit_vacancy.py
@@ -1,9 +1,10 @@
 from tkinter import ttk
 import tkinter as tk
 from create_DB import get_cursor
-from sqlite3 import Cursor
+import customtkinter as ctk
 
-class EditVacancy(tk.Toplevel):
+
+class EditVacancy(ctk.CTkToplevel):
     """
     Всплывающее окно для редактирования вакансий
     """
@@ -13,9 +14,14 @@ class EditVacancy(tk.Toplevel):
 
         self.resizable(width=False, height=False)
 
-        self.id, self.job_title, self.salary, self.department, self.date_opening, self.date_closing = data
-
-        print(data)
+        (
+            self.id,
+            self.job_title,
+            self.salary,
+            self.department,
+            self.date_opening,
+            self.date_closing,
+        ) = data
 
         self.__add_widgets()
 
@@ -24,72 +30,81 @@ class EditVacancy(tk.Toplevel):
         Добвляем виджеты
         """
 
-        ttk.Label(self, text='Редактирование вакансий', font="Arial 24").pack()
+        ctk.CTkLabel(self, text="Редактирование вакансий", font=("Arial", 28)).pack(
+            padx=10, pady=20
+        )
 
-        frame = ttk.Frame(self)
-        frame.pack()
+        frame = ctk.CTkFrame(self)
+        frame.pack(pady=20)
 
         self.__add_entreis(frame)
         self.__add_labels(frame)
 
-        ttk.Button(self, text="Применить", command=self.__edit_vacancy).pack(pady=20)
+        ctk.CTkButton(self, text="Применить", command=self.__edit_vacancy).pack()
 
     def __edit_vacancy(self):
         cursor = get_cursor()
 
-        cursor.execute(f"""
+        cursor.execute(
+            f"""
                         SELECT id FROM job_title
                         WHERE name = "{self.StringVar_job_title.get()}"
-                        """)
-        
+                        """
+        )
+
         job_title_id = list(cursor.fetchone())[0]
 
-        cursor.execute(f"""
+        cursor.execute(
+            f"""
                         SELECT id FROM department
                         WHERE name = "{self.StringVar_depatment.get()}"
-                        """)
-        
+                        """
+        )
+
         department_id = list(cursor.fetchone())[0]
 
-
-        cursor.execute(f"""
+        cursor.execute(
+            f"""
                         UPDATE job_vacancy
                         SET id_job_title = {job_title_id},
                             id_department = {department_id},
                             vacancy_opening_date = "{self.StringVar_opening_date.get()}",
                             vacancy_closing_date = "{self.StringVar_closing_date.get()}"
                         WHERE id = {self.id}
-                        """)
-        
+                        """
+        )
 
-        cursor.execute(f"""
+        cursor.execute(
+            f"""
                         UPDATE job_title
                         SET salary = {self.StringVar_salary.get()}
                         WHERE id = {job_title_id}
-                        """)
-        
+                        """
+        )
 
         cursor.connection.commit()
         self.destroy()
-        
 
-
-    def __add_entreis(self, frame: ttk.Frame):
+    def __add_entreis(self, frame: ctk.CTkFrame):
         """
         Добавляем поля для ввода
         """
         from usefull_functions import JOB_TITLE, DEPARTMENTS
 
         self.StringVar_job_title = tk.StringVar(frame, value=self.job_title)
-        job_title = ttk.OptionMenu(frame, self.StringVar_job_title, self.job_title, *JOB_TITLE)
+        job_title = ctk.CTkOptionMenu(
+            frame, variable=self.StringVar_job_title, values=JOB_TITLE
+        )
         job_title.grid(row=0, column=1)
 
         self.StringVar_salary = tk.StringVar(frame, value=self.salary)
         entry_salary = ttk.Entry(frame, textvariable=self.StringVar_salary)
         entry_salary.grid(row=1, column=1)
 
-        self.StringVar_depatment = tk.StringVar(frame)
-        department = ttk.OptionMenu(frame, self.StringVar_depatment, self.department, *DEPARTMENTS)
+        self.StringVar_depatment = tk.StringVar(frame, value=self.department)
+        department = ctk.CTkOptionMenu(
+            frame, variable=self.StringVar_depatment, values=DEPARTMENTS
+        )
         department.grid(row=2, column=1)
 
         self.StringVar_opening_date = tk.StringVar(frame, value=self.date_opening)
@@ -100,13 +115,21 @@ class EditVacancy(tk.Toplevel):
         entry_closing_date = ttk.Entry(frame, textvariable=self.StringVar_closing_date)
         entry_closing_date.grid(row=4, column=1)
 
-    def __add_labels(self, frame: ttk.Frame):
+    def __add_labels(self, frame: ctk.CTkFrame):
         """
         Добавляем надписи
         """
 
-        ttk.Label(frame, text="Должность: ").grid(row=0, column=0)
-        ttk.Label(frame, text="Зарплата: ").grid(row=1, column=0)
-        ttk.Label(frame, text="Отдел: ").grid(row=2, column=0)
-        ttk.Label(frame, text="Дата открытия: ").grid(row=3, column=0)
-        ttk.Label(frame, text="Дата закрытия: ").grid(row=4, column=0)
+        labels = [
+            "Должность: ",
+            "Зарплата: ",
+            "Отдел: ",
+            "Дата открытия: ",
+            "Дата закрытия: ",
+        ]
+
+        counter = 0
+
+        for label in labels:
+            ctk.CTkLabel(frame, text=label).grid(row=counter, column=0, padx=5, pady=3)
+            counter += 1
